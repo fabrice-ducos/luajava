0641ab8b5086a7dad8aeb10395c9a98e335cffb7
Fabrice Ducos <fabrice.ducos@gmail.com>
2022-06-06 17:12:31 +0200

    src/bash/luajava.sh: now supports a -q (quiet) option

--- src/bash/luajava.sh
+++ src/bash/luajava.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 LUAJAVA_VERSION=__LUAJAVA_VERSION__
+quiet_mode=false
+
+if [ "$1" = '-q' ] ; then
+  quiet_mode=true
+  shift
+fi 
 
 # the prefix is the parent directory of bin, lib, etc
 DEFAULT_PREFIX=`dirname $0`/..
@@ -15,12 +21,13 @@ if [ -f "${LIBDIR}/${JAR_FILE}" ] ; then
 fi
 
 if [ -f "${MAVEN_REPO}/${JAR_FILE}" ] ; then
-  if [ -f "$JAR_PATH" ] ; then
+  if [[ -f "$JAR_PATH" && $quiet_mode = false ]] ; then
     echo "$0: warning: two copies of ${JAR_FILE} have been found:" 1>&2 ;
     echo "$0: warning: - ${LIBDIR}/${JAR_FILE}" 1>&2 ;
     echo "$0: warning: - ${MAVEN_REPO}/${JAR_FILE}" 1>&2 ;
     echo "$0: warning: I will choose ${MAVEN_REPO}/${JAR_FILE}" 1>&2 ;
-    echo "$0: warning: in order to remove this warning, just delete the unwanted copy (the .jar, not the dll|so|dylib file!)" 1>&2
+    echo "$0: warning: in order to remove this warning, run this command in quiet mode (-q)"
+    echo "$0: warning: or delete the unwanted copy (the .jar, not the dll|so|dylib file!)" 1>&2
   fi
   
   # it would be desirable to link against the dynamic library in the maven repo,
