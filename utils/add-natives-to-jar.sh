c35cf6ba9134e7c222c91d8624b8be13292fdf15
fducos <fducos>
2024-09-11 01:04:55 +0200

    utils/add-natives-to-jar.sh updated

--- utils/add-natives-to-jar.sh
+++ utils/add-natives-to-jar.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 usage() {
-    echo "usage: $0 <jar_file> <directory_to_append>" 1>&2
+    echo "usage: $0 <jar_file> <directory>" 1>&2
     echo "  jar_file: the path to the JAR file" 1>&2
-    echo "  directory_to_append: the path to the directory containing the native libraries" 1>&2
-    echo "example: $0 myapp.jar natives" 1>&2
-    echo "This script adds the native libraries in the specified directory to the JAR file." 1>&2
+    echo "  directory: a directory to add to the JAR file" 1>&2
+    echo "example: $0 build/lib/luajava-x.y.jar native" 1>&2
+    echo "This example adds the native libraries to the JAR file." 1>&2
     exit 1
 }
 
